db93608bdb34a2c9be3dfcbe5fbb0a491f75d55b
= <piotti@mit.edu>
2017-08-02 16:44:17 -0400

    fixed db name, added test cs files

---
+++ dbtest.cs
@@ -0,0 +1,38 @@
+using Npgsql;
+using System;
+
+class DBTest {
+
+    public static void Main() {
+        var connString = "Host=localhost;Username=postgres;Password=JensenRobot321;Database=robot_info";
+
+        using (var conn = new NpgsqlConnection(connString))
+        {
+            conn.Open();
+
+            // Insert some data
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = Console.ReadLine();
+                cmd.ExecuteNonQuery();
+            }
+
+            // Retrieve all rows
+            Console.WriteLine("pres_1:");
+            using (var cmd = new NpgsqlCommand("SELECT pres_1 FROM real_time", conn))
+            using (var reader = cmd.ExecuteReader())
+                while (reader.Read())
+                    Console.WriteLine(reader.GetString(0));
+
+            Console.WriteLine("pres_2:");
+            using (var cmd = new NpgsqlCommand("SELECT pres_2 FROM real_time", conn))
+            using (var reader = cmd.ExecuteReader())
+                while (reader.Read())
+                    Console.WriteLine(reader.GetString(0));
+
+
+        }
+
+    }  
+}
